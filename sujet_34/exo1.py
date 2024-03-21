e2e0a7b34fb93b0cde0b9222517d0f9b882ac6f0
nocteln <eliottmieze@gmail.com>
2024-03-21 08:14:09 +0100

    correxion exo 1 du sujet 34

--- sujet_34/exo1.py
+++ sujet_34/exo1.py
@@ -1,7 +1,10 @@
-def moyenne(tab):
-    if len(tab) == 0:
-        return "Tableau vide"
-    else:
-        return sum(tab)/ len(tab)
-    
-print(moyenne([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+def nbrOccurences(str):
+    res = {}
+    for k in range(len(str)):
+        if str[k] in res:
+            res[str[k]] += 1
+        else:
+            res[str[k]] = 1
+    return res
+
+print(nbrOccurences('Hello world !'))
