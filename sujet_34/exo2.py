5c688b87b6b8d8412a27d20f6c7fdd8c59e47f18
nocteln <eliottmieze@gmail.com>
2024-03-21 08:28:59 +0100

    correxion exo 2 sujet 34

--- sujet_34/exo2.py
+++ sujet_34/exo2.py
@@ -1,17 +1,30 @@
-def tri(tab):
-    # i est le premier indice de la zone non triee,
-    # j est le dernier indice de cette zone non triÃ©e. 
-    # Au debut, la zone non triee est le tableau complet.
-    i= 0
-    j= len(tab)-1
-    while i != j :
-        if tab[i] == 0:
-            i= i+1
-        else :
-            valeur = tab[j]
-            tab[j] = tab[i]
-            tab[i] = valeur
-            j= j-1
-    return tab
+def fusion(tab1,tab2):
+    '''Fusionne deux tableaux triés et renvoie
+    le nouveau tableau trié.'''
+    n1 = len(tab1)
+    n2 = len(tab2)
+    tab12 = [0] * (n1 + n2)
+    i1 = 0
+    i2 = 0
+    i = 0
+    while i1 < n1 and i2 < n2: 
+        if tab1[i1] < tab2[i2]:
+            tab12[i] = tab1[i1]
+            i1 = i1 + 1
+        else:
+            tab12[i] = tab2[i2]
+            i2 = i2 + 1
+        i += 1
+    while i1 < n1:
+        tab12[i] = tab1[i1]
+        i1 = i1 + 1
+        i = i + 1 
+    while i2 < n2:
+        tab12[i] = tab2[i2]
+        i2 = i2 + 1
+        i = i + 1 
+    return tab12
 
-print(tri([0, 1, 0, 1, 0, 1, 0, 1, 0]))
+print(fusion([1, 6, 10],[0, 7, 8, 9]))
+print(fusion([1,2,3],[]))
+
